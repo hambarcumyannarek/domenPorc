92904eb124befaafa28d01ec963c4fed1529549b
hambarcumyannarek <narmenia878@gmail.com>
2023-11-03 20:43:46 +0400

    new porc

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
-
+const { createPool } = require('mysql2/promise');
 const app = express();
+
+const pool = createPool({
+    host: process.env.HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    user: process.env.USER
+})
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.render('home.ejs', { name: "Narek" })
+app.get('/', async (req, res) => {
+    const users = await pool.query('select * from users');
+    console.log(users[0][0])
+    res.render('home.ejs', { name: users[0][0].name, lastName: users[0][0].lastname })
 })
 
 app.listen(process.env.PORT, function () {
